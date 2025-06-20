0707d26d68ac8745ee2c5de0191cd3a0d179ab8a
Factories-git <official.factories@gmail.com>
2025-06-20 19:08:58 +0900

    Changes

--- 인간-컴퓨터 상호작용.py
+++ 인간-컴퓨터 상호작용.py
@@ -1,14 +1,18 @@
-import sys, copy
+import sys
 
 input = sys.stdin.readline
 
 k = input().strip()
-prefix = [{i : 0 for i in 'abcdefghijklmnopqrstuvwxyz'}]
-for i in range(len(k)):
-    string = copy.deepcopy(prefix[-1])
-    string[k[i]] += 1
-    prefix.append(string)
 n = int(input())
+prefix = [[0] * (len(k) + 1) for _ in range(26)]
+
+for i in range(1, len(k) + 1):
+    ch = ord(k[i-1]) - ord('a')
+    for j in range(26):
+        prefix[j][i] = prefix[j][i-1]
+    prefix[ch][i] += 1
+
 for i in range(n):
     a, l, r = map(str, input().split())
-    print(prefix[int(r)+1][a] - prefix[int(l)][a])
+    idx = ord(a) - ord('a')
+    print(prefix[idx][int(r)+1] - prefix[idx][int(l)])
